b286609abb368dffdfde916ca3688913ba2a2763
Zahid Hasan <45768533+zahidhasann88@users.noreply.github.com>
2021-02-12 20:07:39 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Find The Largest Number
+//Nested if statement
+
+/*
+var n1 = prompt("First Number: ");
+var n2 = prompt("Second Number: ");
+var n3 = prompt("Third Number: ");
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+n3 = parseInt(n3);
+
+if(n1 >= n2) {
+    if (n1 >= n3) {
+        console.log(n1 + " is the largest");
+    }
+    else{
+        console.log(n3 + " is the largest");
+    }
+}else {
+    if (n2 >= n3) {
+        console.log(n2 + " is the largest");
+    }
+    else {
+        console.log(n3 + " is the largest");
+    }
+}
+*/
