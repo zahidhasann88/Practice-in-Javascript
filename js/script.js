08f7d2198a224cd4d8d49af33cd6e8f157219aa1
Zahid Hasan <45768533+zahidhasann88@users.noreply.github.com>
2021-02-13 11:42:24 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -125,3 +125,18 @@ if (num1Con || num2Con || optionCon) {
     }
 }
 */
+//5: Sum of the series of square numbers!
+/*
+var n = parseInt(prompt("Enter the number of terms: "));
+var sum = 0;
+var series = "";
+
+for (var i = 1; i <= n; i++) {
+    sum += i ** 2;
+    series += (i ** 2).toString();
+    if (i == n) { continue; }
+    series += " + ";
+    
+}
+console.log(`${series} = ${sum}`);
+*/
